83a922ee3214c8b885944ef502947de5893abe2a
Laurent Coustet <laurent.coustet@clarisys.fr>
2017-01-04 18:09:01 +0100

    Release v0.25.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 project = dict(
     name = 'python-poppler-qt4',
-    version = '0.24.0',
+    version = '0.25.0',
     description = 'A Python binding to Poppler-Qt4',
     long_description = (
         'A Python binding to Poppler-Qt4 that aims for '
